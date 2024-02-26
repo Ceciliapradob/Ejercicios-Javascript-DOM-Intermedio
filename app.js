3a646d8a6caf9287ba8a6e9315ebe1b0a1c0d93f
Ceciliapradob <ceciliapradob0799@gmail.com>
2024-02-25 21:30:52 -0300

    Culminado

---
+++ app.js
@@ -0,0 +1,59 @@
+// Ejercicios DOM - Intermedio 
+
+
+// 1- Carrusel de Imágenes:
+// Preparación del HTML:
+// Crea un contenedor para las imágenes y botones de "anterior" y "siguiente".
+// JavaScript para la Interacción:
+// Utiliza un array de URLs de imágenes.
+// Implementa funciones para cambiar la imagen mostrada al hacer clic en los botones de navegación.
+// Añade un temporizador para cambiar automáticamente las imágenes cada cierto tiempo.
+
+
+
+// 2-Slider de Contenido:
+// Preparación del HTML:
+// Crea un contenedor para mostrar contenido deslizable, como imágenes, texto o tarjetas.
+// JavaScript para la Interacción:
+// Organiza el contenido en secciones deslizables.
+// Implementa funciones para moverse entre las diferentes secciones al hacer clic en botones de navegación o al deslizar con el dedo(para dispositivos táctiles).
+
+
+
+// 3-Menú Desplegable:
+// Preparación del HTML:
+// Crea una lista de elementos de menú y un botón para abrir / cerrar el menú.
+// JavaScript para la Interacción:
+// Oculta inicialmente el menú.
+// Implementa una función para mostrar u ocultar el menú al hacer clic en el botón.
+// Añade transiciones suaves para mejorar la experiencia del usuario.
+
+
+
+// 4-Modal o Ventana Emergente:
+// Preparación del HTML:
+// Crea un botón para abrir el modal y un contenedor para el contenido del modal.
+// JavaScript para la Interacción:
+// Oculta inicialmente el modal.
+// Implementa una función para mostrar u ocultar el modal al hacer clic en el botón.
+// Añade la capacidad de cerrar el modal haciendo clic fuera de él o en un botón de cierre.
+
+
+
+
+
+// 5-Barra de Progreso Interactiva:
+// Preparación del HTML:
+// Crea una barra de progreso y botones de "reproducir" y "pausa".
+// JavaScript para la Interacción:
+// Implementa funciones para avanzar y retroceder la barra de progreso al hacer clic en los botones de reproducción y pausa.
+// Añade la funcionalidad de arrastrar la barra de progreso para avanzar o retroceder manualmente.
+
+
+
+
+// Esta actividad se basa en práctica intermedia sobr el DOM, debes tener una base sólida en lo básico de Javascript, conceptos como variables, condicionales, arrays, funciones, eventos, etc.
+
+// Una vez culminado los ejercicios podrás enviarlos vía tu link de repositorio al chat de comunidad slack y podemos darte un feedback al respecto. 
+
+// ¡Éxito!  🩷
